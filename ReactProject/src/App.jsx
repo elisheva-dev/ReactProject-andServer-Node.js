8dbafc6d1d7b186fbcb6699a17bed3953b9d84c7
Elisheva <elisheva4081@gmail.com>
2024-03-14 04:07:29 +0200

    first commit

---
+++ ReactProject/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'  
+// import Login from './login/Login'
+// import { Details } from '@mui/icons-material'
+// import DetailsToShow from './detailsToShow/DetailsToShow'
+
+
+function App() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ ReactProject/src/global/GlobalState.js
@@ -0,0 +1,42 @@
+import { observable, makeObservable, action } from 'mobx';
+
+class GlobalState {
+  isAdmin=false;
+  isServiceOpen=false;
+  isMeetingOpen=false;
+
+    constructor() {
+        makeObservable(this, {
+            isAdmin:observable,
+            setIsAdmin:action,
+            isServiceOpen:observable,
+            isMeetingOpen:observable,
+            setIsServiceOpen:action,
+            setIsMeetingOpen:action
+        })
+    }
+
+    setIsAdmin= (val) => {
+        this.isAdmin = val;
+    }
+    setIsServiceOpen= (val) => {
+        this.isServiceOpen = val;
+        if(val)
+        this.isMeetingOpen=false;
+    }
+    setIsMeetingOpen= (val) => {
+        this.isMeetingOpen = val;
+        if(val)
+        this.isServiceOpen=false;
+    }
+ 
+
+
+
+    
+
+
+
+}
+
+export default new GlobalState();
